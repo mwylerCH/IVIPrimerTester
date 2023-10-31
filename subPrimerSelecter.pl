98cb725cb11b5989ffef4b629b97119d2ea37cc3
mwylerCH <61991286+mwylerCH@users.noreply.github.com>
2023-10-31 09:08:48 +0100

    Update subPrimerSelecter.pl

--- subPrimerSelecter.pl
+++ subPrimerSelecter.pl
@@ -9,65 +9,46 @@ use File::Basename;
 use Cwd;
 use POSIX;
 
+# script to search primers from the list and produce a new subset for subsequent testing.
+# Wyler M. 13.10.2023
 
-# Script to parse output from EMBOSS primersearch
-# Wyler M. 16.10.2023
-
-
-my $PRMSEARCH = $ARGV[0];
+my $TEMPfolder = $ARGV[0];
+my $VIRUS = $ARGV[1];
+# my $VIRUS = "WNV";
+# my $TEMPfolder = "/home/mwyler/tempDevPrimer";
 
 # getwd
-#my $dir = getcwd . "/";
+my $dir = getcwd . "/";
 
 
-### Read File -------------------------------
+# get position of primer file
+my $MACHOPATH = dirname $0;
+my $PRIMERPATH = $MACHOPATH . "/RefFiles/primerIVI.txt";
+my $PRIMERout = $TEMPfolder . "/primerToTest.txt";
 
-# keep array as control to see if multiple marker hit the same sequence
-my @CONTROLERsequence;
-my @OUTput;
-my @ResultRow;
-my %seen;
+# read and filter for marker of interest
+my @PRIMERS;
 
-my $TARGETseq ;
-my $FORWARDmismatch ;
-my $REVERSEmismatch ;
-my $AMPLIMERlength ;
-
-open(IN, $PRMSEARCH) or die "can't open $PRMSEARCH";
+open(IN, $PRIMERPATH) or die "can't open $PRIMERPATH";
 while(<IN>){
 	chomp;
-	# handle raw output
-	if ($_ =~ m/Sequence:/){
-		$TARGETseq = $_ ;
-		$TARGETseq =~ s/\sSequence: //;
-		# test if not already screaned
-#		if ( grep( /^$TARGETseq$/, @CONTROLERsequence ) ) {
-			push(@CONTROLERsequence, $TARGETseq);
-#		}
-	# for and rev mismatch
-	} elsif ($_ =~ m/hits forward strand/) {
-		$FORWARDmismatch = $_;
-		$FORWARDmismatch =~ s/.*\s(\d+)\smismatches/$1/;
-	} elsif ($_ =~ m/hits reverse strand/) {
-                $REVERSEmismatch = $_;
-                $REVERSEmismatch =~ s/.*\s(\d+)\smismatches/$1/;
-	} elsif ($_ =~ m/Amplimer length/) {
-                $AMPLIMERlength = $_;
-                $AMPLIMERlength =~ s/\sAmplimer length:\s//;
-		$AMPLIMERlength =~ s/\sbp//;
-
-		# Print out once reached last row of block (only if problematic)
-		if ($FORWARDmismatch > 1 || $REVERSEmismatch > 1){
-			print "$TARGETseq\tForward=$FORWARDmismatch\tReverse=$REVERSEmismatch\tAmplemerLen=$AMPLIMERlength\n"; 
-		}
-	}
+	if ($_ =~ m/^${VIRUS}.*/){
+		push(@PRIMERS, $_);
+	} 
 }
 close (IN);
 
+# test if primers are found
+my $PRIMERcount = scalar @PRIMERS;
 
-# print warning if multiple hits
-foreach my $string (@CONTROLERsequence) {
-	next unless $seen{$string}++;
-	print "'$string' is duplicated.\n";
-
+if ($PRIMERcount > 0) {
+	open(FH, '>', $PRIMERout) or die $!;
+	foreach (@PRIMERS){
+		print FH "$_\n";
+	}
+	close(FH);
+	print "APPOSTO"
+	
+} else {
+	print "Virus $VIRUS not present in primer list. Check $PRIMERPATH\n";
 }
